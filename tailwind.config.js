2d1f26f4b642aa97fa9887fdf11abd8396dff355
jffry93 <jay.zalischi@gmail.com>
2021-02-19 17:53:34 -0500

    first commitgit status

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    100: '#164A41',
+                    200: '#4D774E',
+                    300: '#9DC88D',
+                },
+                secondary: '#F1B24A',
+            },
+            fontFamily: {
+                chivo: ['Chivo']
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
